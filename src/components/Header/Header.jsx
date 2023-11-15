ff215df15cdc5fe532e8ae51d456d65b4ad38e50
cimafrancamae <cimafrancamae@gmail.com>
2023-11-15 19:23:37 +0800

    feature: header component initial commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,15 +5,12 @@ import logo from '../../../public/slack-logo.png'
 const Header = () => {
   return (
     <Flex justify="space-between" align="center" p="4" boxShadow="md">
-      {/* App Logo */}
       <div>
         <img src={logo} className='header-logo' title='logo' />
       </div>
 
-      {/* Search Bar */}
       <Input placeholder="Search..." maxW="xs" mr="4" />
 
-      {/* User Profile */}
       <Flex align="center">
         <IconButton aria-label="Search notifications" icon={<BellIcon />} mr="4" />
         <Avatar size="sm" name="User" />
