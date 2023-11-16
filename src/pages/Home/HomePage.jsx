827b68c814c70fcb26e02e88c5e22d5d8b162a8f
cimafrancamae <cimafrancamae@gmail.com>
2023-11-16 19:52:28 +0800

    feat: implement homepage

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import { Outlet } from 'react-router-dom';
 
 function HomePage(props) {
     return (
-        <div>
-            
+        <div className='home-container'>
+            <Header />
+            <Sidebar />
+            <Outlet />
         </div>
     );
 }
