da660c6e73623f676323e8a3005fe9e37d37d61e
cimafrancamae <cimafrancamae@gmail.com>
2023-11-17 00:26:19 +0800

    style: message input and message list components

--- src/components/MessageInput/MessageInput.jsx
+++ src/components/MessageInput/MessageInput.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Box, Input, Button } from '@chakra-ui/react';
+import { MdSend } from 'react-icons/md';
 
 const MessageInput = ({ onSendMessage }) => {
   const [message, setMessage] = useState('');
@@ -16,14 +17,26 @@ const MessageInput = ({ onSendMessage }) => {
   };
 
   return (
-    <Box p="3" borderTop="1px solid" borderColor="gray.300">
+    <Box p="3" borderTop="1px solid" borderColor="gray.300" display="flex">
       <Input
         placeholder="Type your message..."
         value={message}
         onChange={handleInputChange}
+        flex="1"
+        marginRight="2"
+        h="4rem"
       />
-      <Button colorScheme="blue" ml="3" onClick={handleSubmit}>
-        Send
+      <Button
+        colorScheme="blue"
+        aria-label="Send Message"
+        onClick={handleSubmit}
+        h="4rem" 
+        w="4rem" 
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <MdSend size={24} />
       </Button>
     </Box>
   );
