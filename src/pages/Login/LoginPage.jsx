c6825aecc0e27f853c374f477fbd3562c184cc29
cimafrancamae <cimafrancamae@gmail.com>
2023-11-17 01:33:15 +0800

    style: login page

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -11,9 +11,12 @@ import {
   AlertIcon,
   useToast,
   Progress,
+  Image,
+  Flex,
 } from '@chakra-ui/react';
 import { loginUser } from '../../services/api';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import logo from '../../../public/slack-with-name-logo.png';
 
 const LoginPage = () => {
   const [formData, setFormData] = useState({
@@ -74,41 +77,62 @@ const LoginPage = () => {
   return (
     <div className="login-container">
         {loading && <Progress size="xs" isIndeterminate colorScheme='blue' />}
-        <Box w="100%" maxW="400px" mt="10">
-        <Heading as="h2" size="lg" mb="4">
-            Login
-        </Heading>
-        { alertMessage && (
-            <Alert status={alertMessage.type} mb={'4'}>
-                <AlertIcon />
-                {alertMessage.message}
-            </Alert>
-        )}
-        <form onSubmit={handleSubmit}>
-            <FormControl mb="4">
-            <FormLabel>Email</FormLabel>
-            <Input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleInputChange}
-                placeholder="Enter your email"
-            />
-            </FormControl>
-            <FormControl mb="4">
-            <FormLabel>Password</FormLabel>
-            <Input
-                type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleInputChange}
-                placeholder="Enter your password"
-            />
-            </FormControl>
-            <Button type="submit" colorScheme="blue" mb="4">
-                Login
-            </Button>
-        </form>
+        <Box 
+          w="100%" 
+          m="auto" 
+          maxW="500px" 
+          mt="10" 
+          textAlign="center"
+        >
+        <Flex align="center" justify="center" direction="column" gap="10">
+          <Image 
+            src={logo} 
+            alt="Slack App Logo" 
+            mb="4" 
+            maxW="100px" 
+          />
+          <Heading as="h2" size="lg" mb="4">
+              Login
+          </Heading>
+          { alertMessage && (
+              <Alert status={alertMessage.type} mb={'4'} maxW="400px">
+                  <AlertIcon />
+                  {alertMessage.message}
+              </Alert>
+          )}
+          <form onSubmit={handleSubmit}>
+              <FormControl mb="4">
+              <FormLabel>Email</FormLabel>
+              <Input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  placeholder="Enter your email"
+                  w="400px"
+              />
+              </FormControl>
+              <FormControl mb="4">
+              <FormLabel>Password</FormLabel>
+              <Input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  placeholder="Enter your password"
+              />
+              </FormControl>
+              <Button type="submit" colorScheme="blue" mb="4" w="100%">
+                  Login
+              </Button>
+            </form>
+            <Text mb="4">
+            {"Don't have an account? "}
+              <Link to="/register" ml="1">
+                Create one here
+              </Link>
+            </Text>
+        </Flex>
         </Box>
     </div>
   );
