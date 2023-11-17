bcb79fa21f3e772899fd78fa79f6f0b72c2aa7b0
cimafrancamae <cimafrancamae@gmail.com>
2023-11-18 00:39:48 +0800

    feat: fetch all users

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../../components/Header/Header';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import MessageList from '../../components/MessageList/MessageList';
 import MessageInput from '../../components/MessageInput/MessageInput';
 import { Flex, Box } from '@chakra-ui/layout';
+import { fetchAllUsers } from '../../services/api';
+import useFetch from '../../utils/hooks/useFetch';
 
 function HomePage(props) {
+    const { apiUrl, options } = fetchAllUsers();
+    const { data, error, loading } = useFetch(apiUrl, options);
+    
+    useEffect(() => {
+      console.log('home',{apiUrl, options});
+    if (loading) {
+      console.log('Loading...');
+    }
+
+    if (error) {
+      console.error('Error:', error.message);
+    }
+
+    if (data) {
+      console.log('All Users:', data);
+    }
+  }, []);
+
     return (
         <div className='home-container'>
             <Header />
