fbadadc688c23be31fedf1df15e728036e8f21a3
cimafrancamae <cimafrancamae@gmail.com>
2023-11-20 02:12:22 +0800

    feat: implement logout

---
+++ src/components/Header/components/LogoutPopover.jsx
@@ -0,0 +1,72 @@
+import {
+  Button,
+  PopoverHeader,
+  PopoverBody,
+  Avatar,
+  Menu,
+  MenuButton,
+  Flex,
+  Text
+} from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+
+function LogoutPopover({ signedInUser, handlePopOver }) {
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.clear();
+        navigate('/');
+        handlePopOver();
+    };
+
+    const handleDarkMode = () => {
+
+    }
+    
+    return (
+        <>
+            <PopoverHeader border="1px solid" borderColor="gray.300">
+                <Flex 
+                    gap="5px"
+                    alignItems="center"
+                    paddingY="2"
+                >
+                    <Avatar name={signedInUser} size="sm" />
+                    <Text fontSize="md" fontWeight="bold">{signedInUser}</Text>
+                </Flex>
+            </PopoverHeader>
+            <PopoverBody>
+            <Menu>
+                <Flex 
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="flex-start"
+                >
+                    <MenuButton 
+                        as={Button} 
+                        variant="ghost" 
+                        w="100%" 
+                        textAlign="left" 
+                        onClick={handleDarkMode}
+                    >
+                        Dark Mode
+                    </MenuButton>
+                    <MenuButton 
+                        as={Button} 
+                        variant="ghost" 
+                        w="100%" 
+                        textAlign="left" 
+                        onClick={handleLogout}
+                    >
+                        Sign Out
+                    </MenuButton>
+                </Flex>
+            </Menu>
+            </PopoverBody>
+        </>
+       
+    );
+}
+
+export default LogoutPopover;
