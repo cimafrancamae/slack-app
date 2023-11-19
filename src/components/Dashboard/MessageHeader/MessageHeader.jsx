9f3f8cbad321a680c7dfec637a4ffc349bab75a6
cimafrancamae <cimafrancamae@gmail.com>
2023-11-19 14:38:51 +0800

    refactor: send direct message

--- src/components/Dashboard/MessageHeader/MessageHeader.jsx
+++ src/components/Dashboard/MessageHeader/MessageHeader.jsx
@@ -1,33 +1,12 @@
-import { Box, Flex, Text, Input, List, ListItem, Avatar, InputLeftElement, InputGroup, InputLeftAddon } from '@chakra-ui/react';
-import React, { useState } from 'react';
-import { flattenArray } from '../../../utils/helper';
+import { Box, Text } from '@chakra-ui/react';
+import SelectUser from './SelectUser';
 
 function MessageHeader({ users }) {
-
-    let filteredUsers = users;
-
+    
     const channel = localStorage.getItem('channel') || null;
     const directMessage = localStorage.getItem('direct-message') || null;
 
     const title = channel ? channel : directMessage ? directMessage : 'New Message';
-
-    const [searchQuery, setSearchQuery] = useState('');
-    const [selectedUser, setSelectedUser] = useState(null);
-
-    if(users){
-        const flattenedUsers = flattenArray(users.data);
-        filteredUsers = flattenedUsers.filter((user) => {
-            user.name = user.email.split('@')[0];       
-            return user.name.toLowerCase().includes(searchQuery.toLowerCase())
-        });
-    }
-
-
-    const handleSelectedUser = (user) => {
-        const userName = user.email.split('@')[0]
-        setSelectedUser(user.id);
-        setSearchQuery(userName);
-      }
     
     return (
         <>
@@ -43,53 +22,7 @@ function MessageHeader({ users }) {
                     {title}
                 </Text>
             </Box>
-            <Box 
-                bg="gray.200" 
-                boxShadow="md"
-                paddingX="4"
-                paddingY="2"
-                borderBottom="1px solid"
-                borderColor="gray.300"
-            >
-                <InputGroup size="xs" borderColor="gray.300">
-                    <InputLeftAddon 
-                        pointerEvents="none" 
-                        color="gray.500"
-                        children="To: " 
-                    />
-                    <Input
-                        type='text'
-                        placeholder="Search users..."
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                    />
-                </InputGroup>
-                <Box 
-                    overflowY="auto" 
-                    h="auto"
-                    maxH="200px"
-                >
-                    {!selectedUser && (
-                        <List>
-                            {filteredUsers ? filteredUsers.map((user) => (
-                            <ListItem 
-                                key={user.id} 
-                                cursor="pointer" 
-                                onClick={()=>handleSelectedUser(user)}
-                                display="flex"
-                                alignItems="center"
-                                _hover={{ bgColor: 'gray.100' }}
-                                gap="10px"
-                                paddingY="2"
-                            >
-                                <Avatar name={user.name} size="xs" />
-                                <Text fontSize="sm">{user.name}</Text>
-                            </ListItem>
-                            )) : <Text fontSize="xs">No users found</Text>}
-                        </List>
-                    )}
-                </Box>
-            </Box>
+            <SelectUser users={users} />
         </>
     );
 }
---
+++ src/components/Dashboard/MessageHeader/SelectUser.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import { Box, Text, Input, List, ListItem, Avatar, InputGroup, InputLeftAddon } from '@chakra-ui/react';
+import { flattenArray } from '../../../utils/helper';
+
+function SelectUser({ users }) {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [selectedUser, setSelectedUser] = useState(null);
+
+    let filteredUsers = users;
+
+    if(users){
+        const flattenedUsers = flattenArray(users.data);
+        filteredUsers = flattenedUsers.filter((user) => {
+            user.name = user.email.split('@')[0];       
+            return user.name.toLowerCase().includes(searchQuery.toLowerCase())
+        });
+    }
+
+    const handleSearch = (e) => {
+        setSelectedUser(null);
+        setSearchQuery(e.target.value);
+    }
+
+
+    const handleSelectedUser = (user) => {
+        const userName = user.email.split('@')[0]
+        setSelectedUser(user.id);
+        setSearchQuery(userName);
+        console.log(selectedUser)
+      }
+
+    return (
+        <Box 
+                bg="gray.200" 
+                boxShadow="md"
+                paddingX="4"
+                paddingY="2"
+                borderBottom="1px solid"
+                borderColor="gray.300"
+            >
+                <InputGroup size="xs" borderColor="gray.300">
+                    <InputLeftAddon 
+                        pointerEvents="none" 
+                        color="gray.500"
+                        children="To: " 
+                    />
+                    <Input
+                        type='text'
+                        placeholder="Search users..."
+                        value={searchQuery}
+                        onChange={handleSearch}
+                    />
+                </InputGroup>
+                <Box 
+                    overflowY="auto" 
+                    h="auto"
+                    maxH="200px"
+                >
+                    {(searchQuery !== '' && !selectedUser) && (
+                        <List>
+                            {filteredUsers ? filteredUsers.map((user) => (
+                            <ListItem 
+                                key={user.id} 
+                                cursor="pointer" 
+                                onClick={()=>handleSelectedUser(user)}
+                                display="flex"
+                                alignItems="center"
+                                _hover={{ bgColor: 'gray.100' }}
+                                gap="10px"
+                                paddingY="2"
+                            >
+                                <Avatar name={user.name} size="xs" />
+                                <Text fontSize="sm">{user.name}</Text>
+                            </ListItem>
+                            )) : <Text fontSize="xs">No users found</Text>}
+                        </List>
+                    )}
+                </Box>
+            </Box>
+    );
+}
+
+export default SelectUser;
