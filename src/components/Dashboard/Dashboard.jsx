8c6fc94d2b7bdd990ca7617f700bb227e296dab6
cimafrancamae <cimafrancamae@gmail.com>
2023-11-21 01:23:30 +0800

    feat: implement api functions

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,23 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from '@chakra-ui/layout';
-import MessageList from './MessageList/MessageList';
+import MessageList from './MessageDisplay/MessageDisplay';
 import MessageInput from './MessageInput/MessageInput';
 import MessageHeader from './MessageHeader/MessageHeader';
 import useFetch from '../../utils/hooks/useFetch';
 import { sendMessage } from '../../services/api';
+import { Toast } from '@chakra-ui/react';
 
 
 function Dashboard({ signedInUser }) {
     const users = JSON.parse(localStorage.getItem('users'));
-    const [messageBody, setMessageBody] = useState(null);
+    const newMessage = JSON.parse(localStorage.getItem('new-msg'));
 
-    const { apiUrl, options } = sendMessage(messageBody);
+    const [message, setMessage] = useState('');
+    const [requestBody, setRequestBody] = useState({});
+
+    const { apiUrl, options } = sendMessage(requestBody);
     const { data, error, load } = useFetch(apiUrl, options);
 
     const onSendMessage = (message) => {
         console.log(message)
-        setMessageBody(message);
+        setMessage(message);
     }
+
+    useEffect(() => {
+        if(newMessage){
+            const body = {
+                "receiver_id": newMessage.receiver.id,
+                "receiver_class": newMessage.receiver_class,
+                "body": message,
+            }
+
+            setRequestBody(body);
+        }
+    },[requestBody]);
+
+    useEffect(() => {
+        if(error){
+            console.error('Error', error.message);
+            Toast({
+                title: 'Failed to send message',
+                status: 'error',
+                position: 'top',
+                duration: 5000,
+                isClosable: true
+            });
+        }
+    }, [error])
 
     return (
         <>
@@ -29,7 +58,7 @@ function Dashboard({ signedInUser }) {
                 boxShadow="md"
             >
                 <Box>
-                    <MessageHeader users={users} signedInUser={signedInUser} />
+                    <MessageHeader users={users} />
                 </Box>
                 <Box flex="1">
                     <MessageList />
